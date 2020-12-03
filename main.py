e8ac140386c5144471e262614992737d03aaebe0
olawaleibrahim <ibrahim.olawale13@gmail.com>
2020-12-03 13:14:14 +0100

    analyse function

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def read_main():
     return {"message": "Welcome to my First FastAPI api"}
 
 print(app)
+
+@app.get("/article/{article_id}")
+def analyze_article(article_id: int, q: str = None):
+    return {
+        "article id": article_id,
+        "previous id": article_id -1,
+        "q": q
+        }
