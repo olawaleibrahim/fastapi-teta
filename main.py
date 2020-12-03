03ee11866e92913f7ce55e8fdb11f4510e0fc178
olawaleibrahim <ibrahim.olawale13@gmail.com>
2020-12-03 13:49:47 +0100

    post method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from ml import nlp
 
 app = FastAPI()
 
@@ -8,10 +9,24 @@ def read_main():
 
 print(app)
 
+
+@app.post("/article/")
+def analyze_article1(body: dict):
+    return body
+
+
 @app.get("/article/{article_id}")
 def analyze_article(article_id: int, q: str = None):
+    count = 0
+
+    if q:
+        doc = nlp(q)
+        count = len(doc.ents)
+    else:
+        pass
     return {
         "article id": article_id,
         "previous id": article_id -1,
-        "q": q
+        "q": q,
+        "count": count
         }
